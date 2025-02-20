93dfe0a6e26dfa20834fc1f3b8165c0199ad9938
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-02-20 16:03:21 +0700

    .

--- resources/views/livewire/store/dashboard/komponen/kategory-button.blade.php
+++ resources/views/livewire/store/dashboard/komponen/kategory-button.blade.php
@@ -1,23 +1,24 @@
 <div>
-    <div class="card">
-        <div class="p-1 card-body">
-            <div class="m-0 row">
-                @for ($i = 0; $i < 12; $i++)
-                <div class="px-1 col-2 col-sm-2 col-md-2 col-lg-1 col-xl-1">
-                    <div class="mx-0 my-1 card">
-                        <img class="card-img-top"
-                            src="https://static.jakmall.id/2024/06/images/products/25735b/thumbnail/fruity-licious-mango-15ml-liquid-vape-saltnic.jpeg"
-                            alt="Card image cap" style="height: 60px">
-                        <div class="p-1 card-body">
-                            <small>Liquid</small>
-                        </div>
-                    </div>
-                </div>
-
+    <div class="table-responsive">
+        <table>
+            <tr>
+                @for ($i = 0; $i < 24; $i++)
+                    <td>
+                        <a class="btn btn-app">
+                            <i class="fas fa-edit"></i> Edit
+                        </a>
+                    </td>
                 @endfor
-
-            </div>
-
-        </div>
+            </tr>
+            <tr>
+                @for ($i = 0; $i < 24; $i++)
+                    <td>
+                        <a class="btn btn-app">
+                            <i class="fas fa-edit"></i> Edit
+                        </a>
+                    </td>
+                @endfor
+            </tr>
+        </table>
     </div>
 </div>
