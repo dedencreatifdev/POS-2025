ab825ea36fbbe128033b9397c9cc60a8e82333b5
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-02-20 16:33:52 +0700

    .

--- resources/views/livewire/store/dashboard/komponen/kategory-button.blade.php
+++ resources/views/livewire/store/dashboard/komponen/kategory-button.blade.php
@@ -1,19 +1,25 @@
 <div>
     <div class="table-responsive">
-        <table>
-            <tr>
+        <table class="bg-white">
+            {{-- <tr>
                 @for ($i = 0; $i < 24; $i++)
                     <td>
-                        <a class="btn btn-app">
-                            <i class="fas fa-edit"></i> Edit
+                        <a class="m-1 btn btn-app">
+                            <i class="fas fa-edit"></i>
+                            <div>
+                                <img src="https://www.onepoundeliquid.com/cdn/shop/files/Original-ForestFruits.jpg?v=1711628322&width=533"
+                                    class="d-block w-100" alt="slider1">
+                                <br>
+                                Edit
+                            </div>
                         </a>
                     </td>
                 @endfor
-            </tr>
+            </tr> --}}
             <tr>
                 @for ($i = 0; $i < 24; $i++)
                     <td>
-                        <a class="btn btn-app">
+                        <a class="m-1 text-sm btn btn-app">
                             <i class="fas fa-edit"></i> Edit
                         </a>
                     </td>
