15e4a52a39c5386e9f6fcec20b8bb5985c015dce
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-02-20 13:50:37 +0700

    .

--- resources/views/livewire/store/dashboard/komponen/kategory-button.blade.php
+++ resources/views/livewire/store/dashboard/komponen/kategory-button.blade.php
@@ -1,22 +1,22 @@
 <div>
-    <div class="m-0 row">
-        <div class="px-1 col-3 ">
-            <div class="m-0 card">
-                <img class="card-img-top"
-                    src="https://static.jakmall.id/2024/06/images/products/25735b/thumbnail/fruity-licious-mango-15ml-liquid-vape-saltnic.jpeg"
-                    alt="Card image cap" style="height: 65px">
-                <div class="p-1 card-body">123</div>
-            </div>
-        </div>
-        <div class="px-1 col-1">
-            <div class="m-0 card">
-                <div class="p-1 card-body">321</div>
-            </div>
-        </div>
-
-    </div>
     <div class="card">
         <div class="p-1 card-body">
+            <div class="m-0 row">
+                @for ($i = 0; $i < 12; $i++)
+                <div class="px-1 col-2 col-sm-2 col-md-2 col-lg-1 col-xl-1">
+                    <div class="mx-0 my-1 card">
+                        <img class="card-img-top"
+                            src="https://static.jakmall.id/2024/06/images/products/25735b/thumbnail/fruity-licious-mango-15ml-liquid-vape-saltnic.jpeg"
+                            alt="Card image cap" style="height: 65px">
+                        <div class="p-1 card-body">
+                            <small>Liquid</small>
+                        </div>
+                    </div>
+                </div>
+
+                @endfor
+
+            </div>
 
         </div>
     </div>
