92daa342c5683d5f8df9fea14d4d0c2ae87eca99
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-02-21 10:57:16 +0700

    .

--- resources/views/livewire/store/dashboard/komponen/kategory-button.blade.php
+++ resources/views/livewire/store/dashboard/komponen/kategory-button.blade.php
@@ -9,21 +9,6 @@
     </div>
     <div class="table-responsive">
         <table class="h-auto bg-white">
-            {{-- <tr>
-                @for ($i = 0; $i < 24; $i++)
-                    <td>
-                        <a class="m-1 btn btn-app">
-                            <i class="fas fa-edit"></i>
-                            <div>
-                                <img src="https://www.onepoundeliquid.com/cdn/shop/files/Original-ForestFruits.jpg?v=1711628322&width=533"
-                                    class="d-block w-100" alt="slider1">
-                                <br>
-                                Edit
-                            </div>
-                        </a>
-                    </td>
-                @endfor
-            </tr> --}}
             <tr>
                 @foreach ($merk as $item)
                     <td>
--- resources/views/livewire/store/dashboard/komponen/store-produk-promo.blade.php
+++ resources/views/livewire/store/dashboard/komponen/store-produk-promo.blade.php
@@ -8,6 +8,31 @@
             <p>Lihat Semua</p>
         </div>
     </div>
+
+    <div class="table-responsive">
+        <table class="h-auto bg-white">
+            <tr>
+                @foreach ($produks_promo as $item)
+                    <td>
+                        <a href="#">
+                            <div class="mx-1 my-1 card" style="width: 10rem;">
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvCdRF6Uk0-Dc74_oneS-URNGSW1oaarJXpw&s"
+                                    class="card-img-top" alt="{{ $item->name }}">
+                                <div class="p-1 text-center card-body">
+                                    <h5 class="text-sm card-title text-bold text-dark">{{ $item->relProdukDetail->name }}</h5>
+                                    <br>
+                                    <p class="card-text text-dark">{{ $item->relProdukDetail->price }} <br>{{ $item->relProdukDetail->promo_price }}</p>
+
+                                </div>
+                            </div>
+
+                        </a>
+                    </td>
+                @endforeach
+            </tr>
+        </table>
+    </div>
+
     <div class="m-0 row">
         @foreach ($produks_promo as $item)
             <a href="#">
