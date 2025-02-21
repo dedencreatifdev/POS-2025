58ba2140ca402f792a3a3646bde8f8aeae30c1fc
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-02-21 11:34:07 +0700

    .

--- resources/views/livewire/store/footer/footer-index.blade.php
+++ resources/views/livewire/store/footer/footer-index.blade.php
@@ -1,3 +1,15 @@
-<div>
-    A good traveler has no fixed plans and is not intent upon arriving.
+<div class="my-3">
+    <div class="text-center card">
+        <div class="card-header">
+          Featured
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        <div class="card-footer text-muted">
+          2 days ago
+        </div>
+      </div>
 </div>
