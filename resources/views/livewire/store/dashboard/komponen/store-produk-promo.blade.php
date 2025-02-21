757326e060d6a030ef68036d4760815f2aaa3abd
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-02-21 11:09:11 +0700

    .

--- resources/views/livewire/store/dashboard/komponen/store-produk-promo.blade.php
+++ resources/views/livewire/store/dashboard/komponen/store-produk-promo.blade.php
@@ -19,9 +19,20 @@
                                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvCdRF6Uk0-Dc74_oneS-URNGSW1oaarJXpw&s"
                                     class="card-img-top" alt="{{ $item->name }}">
                                 <div class="p-1 text-center card-body">
-                                    <h5 class="text-sm card-title text-bold text-dark">{{ $item->relProdukDetail->name }}</h5>
-                                    <br>
-                                    <p class="card-text text-dark">{{ $item->relProdukDetail->price }} <br>{{ $item->relProdukDetail->promo_price }}</p>
+                                    {{-- <h5 class="text-sm card-title text-bold text-dark">{{ $item->relProdukDetail->name }}</h5>
+                                    <br> --}}
+                                    <p class="card-text text-dark">
+                                        {{ $item->relProdukDetail->name }}
+                                        <br>
+                                        <code class="text-bold">
+                                            <strike>Rp {{ number_format($item->relProdukDetail->price/1000,0) }}K</strike>
+                                        </code>
+                                        {{-- <br> --}}
+                                        <code>
+
+                                            Rp {{ number_format($item->relProdukDetail->promo_price ?? '0',2) }}
+                                        </code>
+                                    </p>
 
                                 </div>
                             </div>
@@ -42,7 +53,8 @@
                             class="card-img-top" alt="...">
                         <div class="p-1 card-body">
                             <h5 class="text-sm card-title text-bold text-dark">{{ $item->relProdukDetail->name }}</h5>
-                            <p class="card-text text-dark">Some quick example text to build on the card title and make up the bulk
+                            <p class="card-text text-dark">Some quick example text to build on the card title and make
+                                up the bulk
                                 of
                                 the card's
                                 content.</p>
